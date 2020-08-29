9a881550768f9f945c800105790a2d3c38573b16
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2020-08-29 16:24:41 -0300

    insert post utilizando o banco nedb

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,41 @@
+let Nedb = require('nedb');
+let db = new Nedb({
+    filename: 'users.db',
+    autoload: true
+});
 
-module.exports = (app) =>{
+module.exports = (app) => {
 
-    app.get('/users', (req,res) =>{
+    app.get('/users', (req, res) => {
 
-        res.statusCode =200;
-        res.setHeader('Content-type','Application/json');
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'Application/json');
         res.json([{
-            users:{
-                name:'Thiago Gomes Oliveira',
-                email:'thiago.blu1988@gmail.com',
-                id:1
+            users: {
+                name: 'Thiago Gomes Oliveira',
+                email: 'thiago.blu1988@gmail.com',
+                id: 1
             }
         }])
     });
-    
-    
-    app.post('/users', (req,res) =>{
-    
-        res.json(req.body);
+
+
+    app.post('/users', (req, res) => {
+
+        db.insert(req.body, (err, user) => {
+
+            if (err) {
+                res.status(400);
+                res.json({ error: err })
+                console.log("error", err);
+            } else {
+                res.status(200);
+                res.json(user)
+            }
+
+        })
+
+
     });
 
 }
