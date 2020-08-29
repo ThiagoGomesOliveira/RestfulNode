b1e1a0715a4bcb31e3a685e1243984d1b9bc6a6a
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2020-08-29 16:08:49 -0300

    criando rota post, inclusão do body-parser e banco nedb

--- routes/users.js
+++ routes/users.js
@@ -15,17 +15,9 @@ module.exports = (app) =>{
     });
     
     
-    app.get('/users/admin', (req,res) =>{
+    app.post('/users', (req,res) =>{
     
-        res.statusCode =200;
-        res.setHeader('Content-type','Application/json');
-        res.json([{
-            admin:{
-                name:'Thiago Gomes Oliveira',
-                email:'thiago.blu1988@gmail.com',
-                id:1
-            }
-        }])
+        res.json(req.body);
     });
 
 }
