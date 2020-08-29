5e1ee44a3fcd85072ec7342e4e50852993e33f13
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2020-08-29 16:45:25 -0300

    Buscando os dados do banco com o método Get

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,22 @@ module.exports = (app) => {
 
     app.get('/users', (req, res) => {
 
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'Application/json');
-        res.json([{
-            users: {
-                name: 'Thiago Gomes Oliveira',
-                email: 'thiago.blu1988@gmail.com',
-                id: 1
+         // comando equivalente ao select sem where no banco e ordenando pelo nome.
+        db.find({}).sort({name: 1}).exec((err,users) =>{
+
+            if(err){
+                console.log('error'+ err);
+                res.status(400);
+                res.json({error: err});
+            }else{
+                res.status(200);
+                res.setHeader('Content-type', 'Application/json');
+                res.json([{users}])
             }
-        }])
+
+        })
+
+      
     });
 
 
